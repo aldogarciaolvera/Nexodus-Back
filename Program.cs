7dec5d5b724875ebeeb4f1b951a38ce30435076e
Siniestros Desarrollo <siniestros-desarrollo@devsecops.hdi.com.mx>
2026-09-12 16:56:41 -0600

    [AGO] - main - mnto: Scalar options

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -10,6 +12,12 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference("", options =>
+    {
+        options.HideSearch = true;
+        options.HideModels = true;
+        options.HideDarkModeToggle = true;
+    });
 }
 
 app.UseHttpsRedirection();
